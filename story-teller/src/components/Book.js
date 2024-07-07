22719128ddad1857355a9f15ccbeac8af9ce362b
Duduab <44411900+Duduab@users.noreply.github.com>
2024-07-07 21:52:28 +0300

    Add files via upload
    First version of the App

---
+++ story-teller/src/components/Book.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Page from './Page';
+
+const Book = () => {
+  return (
+    <div>
+      <h1>Story Book</h1>
+      <Page />
+    </div>
+  );
+};
+
+export default Book;
---
+++ story-teller/src/components/Page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div>
+      <h2>Page Content Goes Here</h2>
+      {/* Add page content and navigation here */}
+    </div>
+  );
+};
+
+export default Page;
